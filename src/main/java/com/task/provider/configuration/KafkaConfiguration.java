f6dcff966438d6567e7b7d5bdd7aad0fb96ce34b
Balakin Ilya <victorieuse48@gmail.com>
2020-05-29 12:03:07 +0300

    Fix Kafka configuration.

--- src/main/java/com/task/provider/configuration/KafkaConfiguration.java
+++ src/main/java/com/task/provider/configuration/KafkaConfiguration.java
@@ -1,24 +1,40 @@
 package com.task.provider.configuration;
 
+import lombok.Setter;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableConfigurationProperties
 public class KafkaConfiguration {
 
     @Bean
+    @ConfigurationProperties("kafka")
+    KafkaConsumerProperties kafkaProperties() {
+        return new KafkaConsumerProperties();
+    }
+
+    @Bean
     @RefreshScope
-    public KafkaSender<String, String> kafkaSender(@Value("kafka") Map<String, Object> properties) {
-        var consumerOptions = SenderOptions.<String, String>create(properties)
+    public KafkaSender<String, String> kafkaSender(KafkaConsumerProperties kafkaProperties) {
+        var consumerOptions = SenderOptions.<String, String>create(kafkaProperties.properties)
             .withValueSerializer(new StringSerializer())
             .withKeySerializer(new StringSerializer());
         return KafkaSender.create(consumerOptions);
+    }
+
+    @Setter
+    public static class KafkaConsumerProperties {
+
+        public Map<String, Object> properties = new HashMap<>();
     }
 }
