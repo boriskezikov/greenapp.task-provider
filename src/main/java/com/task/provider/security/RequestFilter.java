69f3abd2f60e88ed55c18a13e3fdc70b71b811ac
boriskezikov <boris200898@icloud.com>
2020-06-01 23:53:52 +0300

    temp logging

--- src/main/java/com/task/provider/security/RequestFilter.java
+++ src/main/java/com/task/provider/security/RequestFilter.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.Objects;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -14,6 +12,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Objects;
+import java.util.Scanner;
 
 import static java.util.Objects.isNull;
 
@@ -28,11 +30,12 @@ public class RequestFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         var authHeader = req.getHeader("X-GREEN-APP-ID");
 
-        LOG.info("-------------------------------------------------------------------------------------------");
-        LOG.info(" /" + req.getMethod());
-        LOG.info(" Request: " + req.getRequestURI());
-        LOG.info("-------------------------------------------------------------------------------------------");
+//        LOG.info("-------------------------------------------------------------------------------------------");
+//        LOG.info(" /" + req.getMethod());
+//        LOG.info(" Request: " + req.getRequestURI());
+//        LOG.info("-------------------------------------------------------------------------------------------");
 
+        logrequest(req);
         if (isNull(authHeader) || !Objects.equals(authHeader, "GREEN")) {
             HttpServletResponse httpResponse = (HttpServletResponse) response;
             httpResponse.setContentType("application/json");
@@ -41,6 +44,41 @@ public class RequestFilter implements Filter {
         }
         chain.doFilter(request, response);
     }
+
+    public void logrequest(HttpServletRequest httpRequest) {
+        LOG.info(" \n\n Headers");
+
+        Enumeration headerNames = httpRequest.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = (String) headerNames.nextElement();
+            LOG.info(headerName + " = " + httpRequest.getHeader(headerName));
+        }
+
+        LOG.info("\n\nParameters");
+
+        Enumeration params = httpRequest.getParameterNames();
+        while (params.hasMoreElements()) {
+            String paramName = (String) params.nextElement();
+            LOG.info(paramName + " = " + httpRequest.getParameter(paramName));
+        }
+
+        LOG.info("\n\n Row data");
+        LOG.info(extractPostRequestBody(httpRequest));
+    }
+
+    static String extractPostRequestBody(HttpServletRequest request) {
+        if ("GET".equalsIgnoreCase(request.getMethod())) {
+            Scanner s = null;
+            try {
+                s = new Scanner(request.getInputStream(), "UTF-8").useDelimiter("\\A");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return s.hasNext() ? s.next() : "";
+        }
+        return "";
+    }
+
 
     @Override
     public void init(FilterConfig filterConfig) {
